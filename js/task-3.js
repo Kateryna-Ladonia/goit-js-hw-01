7a15b61f5c38d8a91f12c355250391e1762a802d
Kateryna-Ladonia <ladonyaku02@gmail.com>
2024-09-20 16:51:21 +0300

    add initial files

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-  return content + 2 * padding + 2 * border;
+  return content + padding + padding + border + border;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
